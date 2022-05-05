9d06bfa868944b1670691ec96b0ce0856544d883
Mark <mark771e@stud.kea.dk>
2022-05-05 10:41:40 +0200

    tag 2 input + totalpris

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         new Main().run();
     }
 
     public void run(){
-
-        System.out.println("hello world");
+        input();
     }
 
+    public void input(){
+        System.out.println("input antal varer");
+        String antalVarer = sc.nextLine();
+        System.out.println("input pris på varen");
+        String varePris = sc.nextLine();
 
-
+        double totalpris = Integer.parseInt(antalVarer)*Double.parseDouble(varePris);
+        System.out.println("totalpris: "+ totalpris + " USD");
+    }
 
 }
